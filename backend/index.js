b50f68f3d2a7289b6c8db43994bb7d523f237704
Tanishq <kawoor.tanishq@gmail.com>
2024-04-29 12:42:38 +0530

    4th commit

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import { dbConnection } from "./database/dbConnection.js";
 
 import messageRouter from "./router/messageRouter.js"
+import checkoutRouter from "./router/checkoutRouter.js"
 
 
 const app = express();
@@ -18,6 +19,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use("/api/v1/message", messageRouter);
+app.use("/api/v1", checkoutRouter);
 
 dbConnection();
 
