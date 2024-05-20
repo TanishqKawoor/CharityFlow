00cdaec68745080945e26c0b2f61d6dc7a48cb32
Tanishq <90197515+TanishqKawoor@users.noreply.github.com>
2024-05-20 21:14:13 +0530

    Update Donate.jsx

--- frontend/src/components/Donate.jsx
+++ frontend/src/components/Donate.jsx
@@ -1,29 +1,80 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 const Donate = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
   const [amount, setAmount] = useState("");
-  const[name,setName] = useState("");
-  const[email, setEmail] = useState("");
-  const[message, setMessage] = useState("");
-  const [disableBtn, setDisableBtn] = useState(false)
+  const [disableBtn, setDisableBtn] = useState(false);
+
+  const handleCheckout = async (e) => {
+    e.preventDefault();
+    try {
+      setDisableBtn(true);
+      await axios
+        .post(
+          "http://localhost:4000/api/v1/checkout",
+          {
+            name,
+            email,
+            message,
+            amount,
+          },
+          {
+            withCredentials: true,
+            headers: { "Content-Type": "application/json" },
+          }
+        )
+        .then((res) => {
+          console.log(res.data);
+          window.location.href = res.data.result.url;
+        });
+    } catch (error) {
+      setDisableBtn(false);
+      console.error(error);
+    }
+  };
+
   return (
-    <section className='donate'>
-      <form>
+    <section className="donate">
+      <form onSubmit={handleCheckout}>
         <div>
           <img src="/logo.png" alt="logo" />
         </div>
         <div>
-          <h1> Show Your Love For Needy</h1>
-          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)}  placeholder='Enter Donation Amount'/>
+          <label>Show your love for Poors</label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter Donation Amount (USD)"
+          />
         </div>
-        <input type="text" value={name} placeholder='Your Name' onChange={(e) => setName(e.target.value)} />
-        <input type="email" value={email} placeholder='Email Address' onChange={(e) => setEmail(e.target.value)} />
-        <textarea rows="10" value={message} placeholder='Your Message' onChange={(e) => setMessage(e.target.value)} />
-        <button type='submit' className='btn' disabled={disableBtn}>Donate {amount ? `Rs ${amount}` : " "}</button>
+        <input
+          type="email"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Your Name"
+        />
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email Address"
+        />
+        <input
+          type="text"
+          placeholder="Message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button type="submit" className="btn" disabled={disableBtn}>
+          Donate {amount ? `$${amount}` : "$0"}
+        </button>
       </form>
-      
     </section>
-  )
-}
+  );
+};
 
-export default Donate
+export default Donate;
